e06dddc9b2c70067ef442e4bb41b8c4fa1cd68d3
Artur Vartsaba <arturvartsaba1@gmail.com>
2025-11-26 17:18:38 +0200

    Ajafunktsioonid

--- content/ajafunktsioonid.php
+++ content/ajafunktsioonid.php
@@ -1,2 +1,27 @@
 <?php
-echo "<h1>Ajafunktsioonid</h1>";
+echo "<h2>Ajafunktsioonid</h2>";
+//ajavööndi lisamine
+date_default_timezone_set('Europe/Tallinn');
+
+echo "Praegune aeg = ".date('G:i' , time());
+
+echo "<br>";
+echo "<br>";
+
+echo "Praegune kuupäev ja aeg = ".date('d.m.Y G:i' , time());
+
+echo "<br>";
+echo "<br>";
+
+echo "Pikk kuupäev = ".date('d.F.Y');
+
+echo "<br>";
+echo "<br>";
+
+//kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevade massiiv
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+echo "Eestistatud pikk kuupäev = ".$paev.'.'.$kuu.' '.$aasta;
