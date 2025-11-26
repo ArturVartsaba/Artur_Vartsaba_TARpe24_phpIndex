33027152ca8306b67a67c376e0fcd8617d1bdd85
Artur Vartsaba <arturvartsaba1@gmail.com>
2025-11-26 17:03:28 +0200

    Dropdown menüü

--- nav.php
+++ nav.php
@@ -1,22 +1,15 @@
-<nav>
+<nav class="menu">
     <ul>
-        <li>
-            <a href="?leht=kodu.php">Kodu</a>
+        <li><a href="?leht=kodu.php">Kodu</a></li>
+        <li><a href="?leht=jsToo2.php">JS küsimustik</a></li>
+        <li><a>PHP Funktsioonid</a>
+            <ul class="dropdown">
+                <li><a href="?leht=Ajafunktsioonid.php">Ajafunktsioonid</a></li>
+                <li><a href="?leht=tekstfunktsioonid.php">Tekstfunktsioonid</a></li>
+                <li><a href="?leht=matemaatilisedFunktsioonid.php>">Matemaatilised</a></li>
+            </ul>
         </li>
-        <li>
-            <a href="?leht=jsToo.php">JS joonistamine</a>
-        </li>
-        <li>
-            <a href="?leht=jsToo2.php">JS küsimustik</a>
-        </li>
-        <li>
-            <a href="?leht=Ajafunktsioonid.php">Ajafunktsioonid</a>
-        </li>
-        <li>
-            <a href="https://arturvartsaba24.thkit.ee" target="_blank">Vana koduleht</a>
-        </li>
-        <li>
-            <a href="?leht=gitKasud.php">GIT käsud</a>
-        </li>
+        <li><a href="https://arturvartsaba24.thkit.ee" target="_blank">Vana koduleht</a></li>
+        <li><a href="?leht=gitKasud.php">GIT käsud</a></li>
     </ul>
 </nav>
